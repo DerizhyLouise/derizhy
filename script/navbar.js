cd20b5f0f27eb99fa20f47c8aeb0670adc30c8c2
DerizhyLouise <louise.derizhy@gmail.com>
2023-12-26 14:22:32 +0700

    responsive and sub title adjustment

--- script/navbar.js
+++ script/navbar.js
@@ -2,23 +2,27 @@ function showNavbar() {
     const body = document.getElementsByTagName('body')[0];
     const navbar = document.createElement('header');
     navbar.innerHTML = `
-        <h2 class="logo"><img class="logo-img" src="../assets/image/logo.png"> Louise Derizhy</h2>
-        <h2 class="logo-small"><img class="logo-img" src="../assets/image/logo.png">LD</h2>
+        <h2 class="logo gradient-text"><img class="logo-img" src="../assets/image/logo.png"> Louise Derizhy</h2>
+        <h2 class="logo-small gradient-text"><img class="logo-img" src="../assets/image/logo.png">LD</h2>
         <nav class="navigation">
             <a href="#home" class='nav-button'>
                 <span class="nav-name">Home</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
             <a href="#profile" class='nav-button'>
-                <span class="nav-name">About</span>
+                <span class="nav-name">Profile</span>
+                <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
+            </a>
+            <a href="#resume" class='nav-button'>
+                <span class="nav-name">Resume</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
             <a href="#project" class='nav-button'>
                 <span class="nav-name">Project</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
-            <a href="#about" class='nav-button'>
-                <span class="nav-name">About</span>
+            <a href="#certification" class='nav-button'>
+                <span class="nav-name">Certification</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
         </nav>
