6c21a5effa4042f1515d43532403e4392bd95297
DerizhyLouise <louise.derizhy@gmail.com>
2024-01-04 15:16:49 +0700

    project part 3

--- script/main.js
+++ script/main.js
@@ -199,4 +199,152 @@ fetch('../json/organizations.json').then(response => response.json()).then(data 
     console.error('Error fetching or processing JSON data:', error);
 });
 
+// Fetch Projects
+fetch('../json/projects.json').then(response => response.json()).then(data => {
+    const projectsContainer = document.querySelector('.project-container');
+
+    data.forEach(project => {
+        const articleWrapper = document.createElement('article');
+        articleWrapper.classList.add('article-wrapper');
+
+        const containerProject = document.createElement('div');
+        containerProject.classList.add('rounded-lg', 'container-project');
+
+        const projectImage = document.createElement('img');
+        projectImage.src = project.img;
+
+        containerProject.appendChild(projectImage);
+
+        const projectInfo = document.createElement('div');
+        projectInfo.classList.add('project-info');
+
+        const projectTitle = document.createElement('div');
+        projectTitle.classList.add('project-title');
+
+        const titleHeading = document.createElement('h3');
+        titleHeading.textContent = project.title;
+
+        projectTitle.appendChild(titleHeading);
+
+        const projectIdInput = document.createElement('input');
+        projectIdInput.classList.add('projectId');
+        projectIdInput.type = 'hidden';
+        projectIdInput.value = project.id;
+
+        const projectButton = document.createElement('a');
+        projectButton.classList.add('project-button');
+        projectButton.textContent = 'More';
+
+        projectInfo.appendChild(projectTitle);
+        projectInfo.appendChild(projectIdInput);
+        projectInfo.appendChild(projectButton);
+
+        articleWrapper.appendChild(containerProject);
+        articleWrapper.appendChild(projectInfo);
+
+        projectsContainer.appendChild(articleWrapper);
+    });
+
+    (function(){
+        var $content = $('.project-modal-info').detach();
+        
+        $('.project-button').on('click', function(e){
+            var projectId = $(this).prev('.projectId').val();
+            
+            fetch('../json/projects.json').then(response => response.json()).then(data => {
+                var project = data.find(item => item.id === parseInt(projectId));
+                if (project) {
+                    const h1 = document.createElement('h1');
+                    const p = document.createElement('p');
+    
+                    h1.textContent = project.title;
+                    p.innerHTML = project.description.join('');
+    
+                    $content.empty();
+                    $content.append(h1, p);
+                }
+    
+                modal.open({
+                    content: $content,
+                    width: '100vh',
+                    height: '100vh',
+                });
+    
+                $content.addClass('modal_content');
+                $('.modal, .project-modal-overlay').addClass('display');
+                $('.project-button').addClass('load');
+            }).catch(error => {
+                console.error('Error fetching or processing JSON data:', error);
+            });
+        });
+    }());
+      
+    var modal = (function(){
+        var $close = $('<button role="button" class="project-modal-close" title="Close"><span></span></button>');
+        var $content = $('<div class="modal_content"/>');
+        var $modal = $('<div class="modal"/>');
+        var $window = $(window);
+      
+        $modal.append($content, $close);
+      
+        $close.on('click', function(e){
+            $('.modal, .project-modal-overlay').addClass('conceal');
+            $('.modal, .project-modal-overlay').removeClass('display');
+            $('.project-button').removeClass('load');
+            e.preventDefault();
+            modal.close();
+        });
+      
+        return {
+            center: function(){
+                var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
+                var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
+                
+                $modal.css({
+                    top: top + $window.scrollTop(),
+                    left: left + $window.scrollLeft(),
+                });
+            }, open: function(settings){
+                const body = document.querySelector('html');
+                const main = document.querySelector('.main');
+                const sidebar = document.querySelector('.side-bar');
+                const navbar = document.querySelector('header');
+                const footer = document.querySelector('footer');
+                body.style.overflow = 'hidden';
+                main.style.filter = 'blur(20px)';
+                sidebar.style.filter = 'blur(20px)';
+                navbar.style.filter = 'blur(20px)';
+                footer.style.filter = 'blur(20px)';
+    
+                $content.empty().append(settings.content);
+        
+                $modal.css({
+                    width: settings.width || '100vh',
+                    height: settings.height || '100vh'
+                }).appendTo('body');
+      
+                modal.center();
+                $(window).on('resize', modal.center);
+            }, close: function(){
+                const body = document.querySelector('html');
+                const main = document.querySelector('.main');
+                const sidebar = document.querySelector('.side-bar');
+                const navbar = document.querySelector('header');
+                const footer = document.querySelector('footer');
+                body.style.overflow = 'scroll';
+                main.style.filter = 'none';
+                sidebar.style.filter = 'none';
+                navbar.style.filter = 'none';
+                footer.style.filter = 'none';
+    
+                $content.empty();
+                $modal.detach();
+                $(window).off('resize', modal.center);
+            }
+        };
+    }());
+}).catch(error => {
+    console.error('Error fetching or processing JSON data:', error);
+});
+
 AOS.init();
