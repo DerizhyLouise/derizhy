bb78ea7a74214e7040c370a8b7f84857f85c825d
DerizhyLouise <louise.derizhy@gmail.com>
2024-01-31 13:12:42 +0700

    fix url

--- script/navbar.js
+++ script/navbar.js
@@ -5,23 +5,23 @@ function showNavbar() {
         <h2 class="logo gradient-text"><img class="logo-img" src="./assets/svg/logo.svg"> Louise Derizhy</h2>
         <h2 class="logo-small gradient-text"><img class="logo-img" src="./assets/svg/logo.svg"></h2>
         <nav class="navigation">
-            <a href="/#home" class='nav-button'>
+            <a href="/profile#home" class='nav-button'>
                 <span class="nav-name">Home</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
-            <a href="/#profile" class='nav-button'>
+            <a href="/profile#profile" class='nav-button'>
                 <span class="nav-name">Profile</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
-            <a href="/#resume" class='nav-button'>
+            <a href="/profile#resume" class='nav-button'>
                 <span class="nav-name">Resume</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
-            <a href="/#project" class='nav-button'>
+            <a href="/profile#project" class='nav-button'>
                 <span class="nav-name">Project</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
-            <a href="/#review" class='nav-button'>
+            <a href="/profile#review" class='nav-button'>
                 <span class="nav-name">Review</span>
                 <svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>
             </a>
