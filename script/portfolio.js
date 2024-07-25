a6433d267ece1a02285ea5436fae313fce38f231
DerizhyLouise <louise.derizhy@gmail.com>
2024-07-25 12:02:58 +0700

    config tailwind theme color

--- script/portfolio.js
+++ script/portfolio.js
@@ -21,7 +21,7 @@ fetch("../assets/json/skills.json")
 			}
 
 			item.innerHTML = `
-				<a href="${skill.link}" target="_blank" class="w-40 h-40 p-4 bg-[#F76C6C] flex flex-col justify-center items-center gap-2 shadow-lg shadow-[#24305E] hover:scale-125 hover:duration-300 transition">
+				<a href="${skill.link}" target="_blank" class="w-40 h-40 p-4 bg-red flex flex-col justify-center items-center gap-2 shadow-lg shadow-darkblue hover:scale-125 hover:duration-300 transition">
 					${iconElement.outerHTML}
 					<h4 class="font-semibold text-center">
 						${skill.skill}
@@ -139,7 +139,7 @@ fetch("../assets/json/certifications.json")
 		data.reverse();
 		data.forEach((certi) => {
 			const certiList = document.createElement("div");
-			certiList.className = `flex group mx-2 border-b-2 border-[#24305E] last:border-0`;
+			certiList.className = `flex group mx-2 border-b-2 border-darkblue last:border-0`;
 
 			if (certi.credentialId) {
 				certiList.innerHTML = `
@@ -151,7 +151,7 @@ fetch("../assets/json/certifications.json")
                         <p class="text-sm sm:text-lg">${certi.issuer}</p>
                         <p class="text-sm sm:text-lg">Issued ${certi.issueDate}</p>
                         <p class="mb-2 text-sm sm:text-lg">Credential ID ${certi.credentialId}</p>
-                        <a class="my-4 h-8 px-4 text-sm sm:text-lg hover:bg-[#F76C6C] duration-300 border-2 border-[#F76C6C]" target='_blank' href="${certi.url}">Show Credential</a>
+                        <a class="my-4 h-8 px-4 text-sm sm:text-lg hover:bg-red duration-300 border-2 border-red" target='_blank' href="${certi.url}">Show Credential</a>
                     </div>
                 `;
 			} else {
@@ -163,7 +163,7 @@ fetch("../assets/json/certifications.json")
                         <h3 class="text-lg sm:text-2xl font-bold">${certi.certificateName}</h3>
                         <p class="text-sm sm:text-lg">${certi.issuer}</p>
                         <p class="mb-2 text-sm sm:text-lg">Issued ${certi.issueDate}</p>
-                        <a class="my-4 h-8 px-4 text-sm sm:text-lg hover:bg-[#F76C6C] duration-300 border-2 border-[#F76C6C]" target='_blank' href="${certi.url}">Show Credential</a>
+                        <a class="my-4 h-8 px-4 text-sm sm:text-lg hover:bg-red duration-300 border-2 border-red" target='_blank' href="${certi.url}">Show Credential</a>
                     </div>
                 `;
 			}
@@ -183,7 +183,7 @@ fetch("../assets/json/reviews.json")
 		data.forEach((review) => {
 			const listItem = document.createElement("a");
 			listItem.className =
-				"h-[416px] w-[416px] max-sm:h-full bg-[#F8E9A1] no-underline flex flex-col p-4 rounded shadow-2xl";
+				"h-[416px] w-[416px] max-sm:h-full bg-yellow no-underline flex flex-col p-4 rounded shadow-2xl";
 			listItem.setAttribute("data-aos", "fade-up");
 			listItem.setAttribute("data-aos-duration", "2000");
 			listItem.href = review.url;
@@ -215,7 +215,7 @@ fetch("../assets/json/projects.json")
 		data.forEach((project) => {
 			const item = document.createElement("button");
 			item.className =
-				"relative group shadow-2xl h-96 w-96 overflow-hidden border-2 border-[#24305E] bg-[#F8E9A1]";
+				"relative group shadow-2xl h-96 w-96 overflow-hidden border-2 border-darkblue bg-yellow";
 			item.onclick = function () {
 				openModal(project.id);
 			};
@@ -224,10 +224,10 @@ fetch("../assets/json/projects.json")
 
 			item.innerHTML = `
                 <div
-                    class="group-hover:-translate-y-full flex justify-center items-center duration-300 ease-in-out w-full h-full bg-[#F8E9A1]">
+                    class="group-hover:-translate-y-full flex justify-center items-center duration-300 ease-in-out w-full h-full bg-yellow">
                     <img src="${project.img}" class="h-full w-full object-cover" alt="${project.title} Image">
                 </div>
-                <div class="bg-[#24305E] p-4 w-full h-full flex justify-center items-center group-hover:-translate-y-full duration-300 ease-in-out">
+                <div class="bg-darkblue p-4 w-full h-full flex justify-center items-center group-hover:-translate-y-full duration-300 ease-in-out">
                     <h2 class="text-4xl font-semibold text-center">${project.title}</h2>
                 </div>
             `;
@@ -257,12 +257,12 @@ function openModal(id) {
                     <h3 class="text-4xl max-md:text-3xl text-white font-bold">${
 						project.title
 					}</h3>
-                    <h3 class="mt-4 mb-4 pt-4 font-semibold text-2xl max-md:text-xl border-t-2 border-[#F76C6C]">Description</h3>
+                    <h3 class="mt-4 mb-4 pt-4 font-semibold text-2xl max-md:text-xl border-t-2 border-red">Description</h3>
                     <p class="text-base mb-5 max-md:text-sm">${project.time}</p>
                     <p class="text-base mb-5 max-md:text-sm">${project.description.join(
 						""
 					)}</p>
-                    <h3 class="mt-4 mb-4 pt-4 font-semibold text-2xl max-md:text-xl border-t-2 border-[#F76C6C]">Project's Media</h3>
+                    <h3 class="mt-4 mb-4 pt-4 font-semibold text-2xl max-md:text-xl border-t-2 border-red">Project's Media</h3>
                 `;
 
 				const projectMedia = document.createElement("div");
@@ -281,19 +281,19 @@ function openModal(id) {
 
 					const list = document.createElement("a");
 					list.className =
-						"relative group shadow-2xl h-60 w-60 overflow-hidden border-2 border-[#24305E] bg-[#F8E9A1]";
+						"relative group shadow-2xl h-60 w-60 overflow-hidden border-2 border-darkblue bg-yellow";
 					list.setAttribute("href", media.url);
 					list.setAttribute("target", "_blank");
 
 					list.innerHTML = `
                         <div
-                            class="group-hover:-translate-y-full text-[#24305E] flex justify-center items-center duration-300 ease-in-out w-full h-full bg-[#F8E9A1]">
+                            class="group-hover:-translate-y-full text-darkblue flex justify-center items-center duration-300 ease-in-out w-full h-full bg-yellow">
                             <div class="flex flex-col items-center justify-center">
                                 <span class="${media.typeSymbol} text-6xl"></span>
                                 <h2 class="text-2xl font-semibold text-center">${media.type}</h2>
                             </div>
                         </div>
-                        <div class="bg-[#F76C6C] text-[#24305E] p-4 w-full h-full flex justify-center items-center group-hover:-translate-y-full duration-300 ease-in-out">
+                        <div class="bg-red text-darkblue p-4 w-full h-full flex justify-center items-center group-hover:-translate-y-full duration-300 ease-in-out">
                             <h2 class="text-2xl font-semibold text-center">${media.title}</h2>
                         </div>
                     `;
@@ -337,7 +337,7 @@ function closeModal() {
 		<div class="p-12 w-full">
 			<div class="absolute right-20 top-5 z-50">
 				<button
-					class="text-white w-14 h-14 max-md:w-12 max-md:h-12 fixed group hover:bg-[#F76C6C] rounded-lg"
+					class="text-white w-14 h-14 max-md:w-12 max-md:h-12 fixed group hover:bg-red rounded-lg"
 					title="Close" onClick="closeModal()">
 					<i class="fa-solid fa-xmark text-2xl group-hover:rotate-180 group-hover:duration-500 transition"></i>
 				</button>
---
+++ script/tailwind.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				darkblue: "#24305E",
+				blue: "#374785",
+				softblue: "#A8D0E6",
+				red: "#F76C6C",
+				yellow: "#F8E9A1",
+			},
+		},
+	},
+};
