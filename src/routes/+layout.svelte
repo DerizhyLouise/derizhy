1a30d6b22fe29cd5d311512ab6a744f3ea088f73
DerizhyLouise <louise.derizhy@gmail.com>
2025-10-09 11:17:19 +0700

    fix: AOS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,16 @@
     import favicon from "$lib/assets/svg/favicon.svg";
     import Footer from "$lib/custom-components/footer.svelte";
     import Header from "$lib/custom-components/header.svelte";
+    import AOS from "aos";
+    import "aos/dist/aos.css";
+    import { onMount } from "svelte";
     import "../app.css";
 
     let { children } = $props();
+
+    onMount(() => {
+        AOS.init();
+    });
 </script>
 
 <svelte:head>
