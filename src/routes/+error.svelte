08ea196107bfaa7fb1aeed025a2c009903e236fd
DerizhyLouise <louise.derizhy@gmail.com>
2025-10-09 15:28:00 +0700

    feat: Error Fallback

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<div>
+    <section class="text-gray h-full min-h-screen w-full">
+        <div class="flex h-screen flex-col items-center justify-center">
+            <h2
+                class="text-center text-4xl font-semibold uppercase sm:text-6xl"
+            >
+                Error
+            </h2>
+            <br />
+        </div>
+    </section>
+</div>
