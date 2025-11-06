80814c02b5698b4597964e06ad1e7448ac4e9f38
DerizhyLouise <louise.derizhy@gmail.com>
2025-11-06 16:43:04 +0700

    feat: Adjust Progress Bar to be more Natural

--- src/lib/custom-components/progress-bar.svelte
+++ src/lib/custom-components/progress-bar.svelte
@@ -1,33 +1,56 @@
 <script lang="ts">
     import { afterNavigate, beforeNavigate } from "$app/navigation";
-    import { onMount } from "svelte";
+    import { onDestroy, onMount } from "svelte";
 
     let progressBar: HTMLDivElement | null = $state(null);
+    let progress: number = $state(0);
+    let interval: ReturnType<typeof setInterval> | null = $state(null);
 
     onMount(() => {
-        progressBar = document.getElementById("progress-bar") as HTMLDivElement;
-
         beforeNavigate(() => {
-            if (progressBar) {
-                progressBar.style.width = "0%";
-                progressBar.style.transition = "none";
-                progressBar.style.transition = "width 0.4s ease";
-                progressBar.style.width = "60%";
-            }
+            if (!progressBar) return;
+
+            progress = 0;
+            progressBar.style.width = "0%";
+            progressBar.style.transition = "width 0.4s ease";
+
+            progress = 40;
+            progressBar.style.width = progress + "%";
+
+            interval = setInterval(() => {
+                if (progress < 90) {
+                    progress += 10;
+                    progressBar!.style.width = progress + "%";
+                }
+            }, 400);
         });
 
         afterNavigate(() => {
-            if (progressBar) {
-                progressBar.style.width = "100%";
-                setTimeout(() => {
-                    progressBar!.style.width = "0%";
-                }, 400);
+            if (!progressBar) return;
+
+            progress = 100;
+            progressBar.style.width = "100%";
+
+            if (interval) {
+                clearInterval(interval);
+                interval = null;
             }
+
+            setTimeout(() => {
+                progress = 0;
+                progressBar!.style.width = "0%";
+            }, 500);
         });
+    });
+
+    onDestroy(() => {
+        if (interval) {
+            clearInterval(interval);
+        }
     });
 </script>
 
 <div
     class="bg-yellow fixed top-0 left-0 z-[9999] h-[3px] w-0 transition-[width] duration-300 ease-linear"
-    id="progress-bar"
+    bind:this={progressBar}
 ></div>
